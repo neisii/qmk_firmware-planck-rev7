728e67b25a73d27f4ad3996a4c5d87a90ef4a640
neisii <neisii@outlook.com>
2025-07-01 09:42:28 +0900

    ver2.1 - add User Functional keycode

---
+++ keymaps/my_v2.1/rules.mk
@@ -0,0 +1,8 @@
+DEFERRED_EXEC_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+KEY_LOCK_ENABLE = yes
+RGB_MATRIX_DRIVER = ws2812
+SEND_STRING_ENABLE = yes
+AUDIO_ENABLE = yes
+CONSOLE_ENABLE = yes         # Console for debug
+BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
